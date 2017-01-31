ae96f511520fe683e0f0948085726cbd16e4b3a3
nishanth28 <pnishanth28@gmail.com>
2017-01-31 12:13:40 -0500

    POST method

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id : 1,
-	description : 'Interview at 12',
-	check : false
-},{
-	id : 2,
-	description : 'Assignment Submission',
-	check : false
-},{
-	id : 3,
-	description : 'Bug fixes and performance improvements on iOS Apps',
-	check : true
-}];
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
+
+app.post('/todos', function (request,response) {
+	var body = request.body;
+	body.id = todoNextId++;
+	todos.push(body); 
+	console.log(body);
+	response.json(body);
+
+});
 
 app.get('/', function (request,response) {
 	response.send('Todo API SETUP: ROOT FOLDER');
 });
 
+//GET/todos
 app.get('/todos', function (request,response) {
 	response.json(todos);
 });
 
+
+//GET/todos/:id
 app.get('/todos/:id', function (request,response) {
 
 	var todoID = parseInt(request.params.id, 10);
